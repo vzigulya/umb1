ac5f5f2cea1543381f30af28a37f6d8b0154e4b8
vzigulya <vazor@mail.ru>
2017-11-03 02:32:38 +0200

    Google Maps Added
    npm install @agm/core --save

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 //First Page Module seems like should be routed or somehow called.
 //FirstPageComponent it shows that it being running using Console.Log but display nothing
@@ -11,8 +14,9 @@ import { TestComponent } from './components/test/test.component';
 import { navItemContent} from './components/nav-item/nav-item';
 import { HeaderComponent} from './components/header/header.component';
 import { NgbdAlertCloseable } from './components/alert-closeable/alert-closeable';
-import { FormsModule }   from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GmapsComponent } from './components/googlemaps/googlemaps.component';
+
+
 
 @NgModule({
   //Declaration of Internal Application Components
@@ -21,11 +25,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
      TestComponent,
      NgbdAlertCloseable,
       navItemContent,
-     HeaderComponent
+     HeaderComponent,
+     GmapsComponent
   ],
   //External Angular and Other Vendors Modules
   imports: [
-
+    AgmCoreModule.forRoot({
+     apiKey: 'AIzaSyCESyzus6Bs7X-5_-38X0bkqJjD9ATRJVI'
+   }),
     BrowserModule,
     HttpModule,
     FormsModule,
---
+++ app/components/googlemaps/googlemaps.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-gmaps',
+  styles: [`
+    agm-map {
+      height: 500px;
+    }
+  `],
+  template: `
+    <agm-map [latitude]="lat" [longitude]="lng">
+      <agm-marker [latitude]="lat" [longitude]="lng"></agm-marker>
+      <agm-marker *ngFor="let m of markers; let i = index"
+          (markerClick)="clickedMarker(m.label, i)"
+          [latitude]="m.lat"
+          [longitude]="m.lng"
+          [label]="m.label"
+          [markerDraggable]="m.draggable">
+          <agm-info-window [disableAutoPan]="true">
+           {{m.info}}
+        </agm-info-window>
+      </agm-marker>
+
+    </agm-map>
+  `,
+})
+
+export class GmapsComponent {
+
+  clickedMarker(label: string, index: number) {
+      console.log(`clicked the marker: ${label || index}`)
+  }
+  // google maps zoom level
+  zoom: number = 8;
+  // initial center position for the map
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+
+  markers: marker[] = [
+        {
+          lat: 51.673858,
+          lng: 7.815982,
+          label: 'A',
+          info: 'Manhattan, Address Line 2',
+          draggable: false
+        },
+        {
+          lat: 51.373858,
+          lng: 7.215982,
+          label: 'B',
+          info: 'New York, Address Line 2',
+          draggable: false
+        },
+        {
+          lat: 51.723858,
+          lng: 7.895982,
+          label: 'C',
+          info: 'New Jersey, Address Line 2',
+          draggable: true
+        }
+      ];
+ constructor(){
+   //attributes which will be used in work
+   this.lat;
+   this.lng;
+   this.zoom;
+   this.markers;
+ }
+
+}
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+  info?:string;
+	draggable: boolean;
+}
